bff78e68315fdda98f1bd0e98fd08c388472a7f4
Akshay Laghate <akki.the.abe@gmail.com>
2015-10-30 11:59:40 +0530

    + Java Lib. + Android Lib. + Intent Extra.

--- jokerandroidlibrary/src/main/java/com/indcoders/jokerandroidlibrary/DisplayActivity.java
+++ jokerandroidlibrary/src/main/java/com/indcoders/jokerandroidlibrary/DisplayActivity.java
@@ -1,13 +1,19 @@
 package com.indcoders.jokerandroidlibrary;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class DisplayActivity extends AppCompatActivity {
+
+    TextView tvJoke;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
+
+        tvJoke = (TextView) findViewById(R.id.tvJoke);
+        tvJoke.setText(getIntent().getStringExtra("joke"));
     }
 }
