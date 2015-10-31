39d72dbf4eeacc0bb2a205ad8db757374ca0d957
Akshay Laghate <akki.the.abe@gmail.com>
2015-10-31 20:18:29 +0530

    + Wired up. + Progress Dialog.

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -1,15 +1,23 @@
 package com.udacity.gradle.builditbigger;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
-import com.ND.JokerClass;
+import com.example.akki.myapplication.backend.myApi.MyApi;
+import com.google.api.client.extensions.android.http.AndroidHttp;
+import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.indcoders.jokerandroidlibrary.DisplayActivity;
+
+import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -44,12 +53,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void tellJoke(View view){
-        Toast.makeText(this, JokerClass.getJoke(), Toast.LENGTH_SHORT).show();
-
-        Intent i = new Intent(this, DisplayActivity.class);
-        i.putExtra("joke", JokerClass.getJoke());
-        startActivity(i);
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Akshay"));
     }
 
+}
 
+class EndpointsAsyncTask extends AsyncTask<Pair<Context, String>, Void, String> {
+    private static MyApi myApiService = null;
+    ProgressDialog pd;
+    private Context context;
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+        pd = new ProgressDialog(context);
+        pd.setIndeterminate(true);
+        pd.setMessage("Retrieving Joke...");
+        pd.show();
+    }
+
+    @Override
+    protected String doInBackground(Pair<Context, String>... params) {
+        if (myApiService == null) {  // Only do this once
+            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(), new AndroidJsonFactory(), null)
+                    .setRootUrl("https://joker-1115.appspot.com/_ah/api/");
+            // end options for devappserver
+
+            myApiService = builder.build();
+        }
+
+        context = params[0].first;
+        String name = params[0].second;
+
+        try {
+            return myApiService.sayHi(name).execute().getData();
+        } catch (IOException e) {
+            return e.getMessage();
+        }
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        pd.dismiss();
+        Toast.makeText(context, result, Toast.LENGTH_LONG).show();
+        Intent i = new Intent(context, DisplayActivity.class);
+        i.putExtra("joke", result);
+        context.startActivity(i);
+
+    }
 }
